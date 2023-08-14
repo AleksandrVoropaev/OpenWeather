d60e566f9506774b045d950fee04b7a23620a14d
AleksandrVoropaev <aleksandr.voropayev@gmail.com>
2023-08-14 13:08:54 +0200

    Project created

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Oleksandr Voropayev on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
